997c3ada4b8d8b91bc60d4874a9d6666a6763612
Enlou <111488176+Enlou@users.noreply.github.com>
2022-08-25 15:33:12 +0800

    Create app.java

---
+++ class_with_constructor/app.java
@@ -0,0 +1,39 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Creates a new object named "Knight" from Character class
+        Character knight = new Character(10, 5, 0, "Chen");
+        // Calls the sayMyName method
+        knight.sayMyName();
+        // Calls the sayMyStats method
+        knight.sayMyStats();
+
+        // Creates a new object named "mage" from Character class
+        Character mage = new Character(0, 5, 10, "Akasha");
+        // Calls the sayMyName method
+        mage.sayMyName();
+        // Calls the sayMyStats method
+        mage.sayMyStats();
+
+        // Creates a new object named "mage" from Character class
+        Character thief = new Character(5, 10, 0, "Gondar");
+        // Calls the sayMyName method
+        thief.sayMyName();
+        // Calls the sayMyStats method
+        thief.sayMyStats();
+        
+        // Creates a new object named "mage" from Character class
+        Character Archer = new Character(8, 10, 6, "Ashe");
+        // Calls the sayMyName method
+        Archer.sayMyName();
+        // Calls the sayMyStats method
+        Archer.sayMyStats();
+
+        // Creates a new object named "mage" from Character class
+        Character Priest = new Character(4, 5, 5, "John");
+        // Calls the sayMyName method
+        Priest.sayMyName();
+        // Calls the sayMyStats method
+        Priest.sayMyStats();
+    }
+}
