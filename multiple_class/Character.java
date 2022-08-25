13b329fdb5285289390aacdafe516087c7e2d9f3
Enlou <111488176+Enlou@users.noreply.github.com>
2022-08-25 15:46:18 +0800

    Create Character.java

---
+++ multiple_class/Character.java
@@ -0,0 +1,23 @@
+public class Character {
+    // Character attributes
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+    int defense;
+
+    // Character method
+    public void sayMyName() {
+        System.out.println("...I'm " + name);
+    }
+    
+    // Character method
+    public void sayMyName2() {
+        System.out.println("Nice to meet you i'm " + name);
+    }
+
+    // Character method
+    public void sayMyName3() {
+        System.out.println("HI! my name is " + name);
+    }
+}
