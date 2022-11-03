dc46536e73620e802e2c619545d564752033aab0
Enlou <111488176+Enlou@users.noreply.github.com>
2022-11-03 16:44:03 +0800

    Update App.java

--- Word Pattern/App.java
+++ Word Pattern/App.java
@@ -20,6 +20,7 @@ public class App {
             }
         }
     }
+
     System.out.println("");
     System.out.println("");
     System.out.println("Word 2");
@@ -31,6 +32,7 @@ public class App {
             }
         }
     }
+
     System.out.println("");
     System.out.println("");
     System.out.println("Word 3");
@@ -42,6 +44,7 @@ public class App {
             }
         }
     }
+
     System.out.println("");
     System.out.println("");
     System.out.println("Word 4");
@@ -119,16 +122,164 @@ public class App {
         }
     }
 
-    System.out.println(" ");
-    System.out.println(" ");
+    System.out.println("");
+    System.out.println("");
     System.out.println("Word 10");
+        for (int row = 3; row >= 0; row--) {
+            // System.out.println("");
+            for (int column = 0; column < 4; column++) {
+                if (column == 0) {
+                    System.out.println(mdArray[row][column]);
+            }
+        }
+    }
+
+    System.out.println("");
+    System.out.println("Word 11");
+        for (int row = 3; row >= 0; row--) {
+            // System.out.println("");
+            for (int column = 0; column < 4; column++) {
+                if (column == 1) {
+                    System.out.println(mdArray[row][column]);
+            }
+        }
+    }
+
+    System.out.println("");
+    System.out.println("Word 12");
+        for (int row = 0; row < 4; row++) {
+            // System.out.println("");
+            for (int column = 0; column < 4; column++) {
+                if ((column == 0 && row == 2) || (column == 1 && row == 2) || (column == 2 && row == 2)) {
+                    System.out.print(mdArray[row][column]);
+            }
+        }
+    }
+
+    System.out.println("");
+    System.out.println("");
+    System.out.println("Word 13");
+        for (int row = 0; row < 4; row++) {
+            // System.out.println("");
+            for (int column = 0; column < 4; column++) {
+                if ((column == 0 && row == 3) || (column == 1 && row == 3) || (column == 2 && row == 3)) {
+                    System.out.print(mdArray[row][column]);
+            }
+        }
+    }
+
+    System.out.println("");
+    System.out.println("");
+    System.out.println("Word 14");
+        for (int row = 0; row < 4; row++) {
+            System.out.println(" ");
+            for (int column = 0; column < 4; column++) {
+                if ((column == 0 && row == 1) || (column == 1 && row == 2) || (column == 2 && row == 3)){
+                    System.out.print(mdArray[row][column]);
+            }
+            else 
+            {
+                System.out.print(" ");
+            }
+        }
+    }
+
+    System.out.println("");
+    System.out.println("");
+    System.out.println("Word 15");
+        for (int row = 3; row >= 0; row--) {
+            System.out.println(" ");
+            for (int column = 3; column >= 0; column--) {
+                if ((column == 0 && row == 1) || (column == 1 && row == 2) || (column == 2 && row == 3)){
+                    System.out.print(mdArray[row][column]);
+            }
+            else 
+            {
+                System.out.print(" ");
+            }
+        }
+    }
+
+    // System.out.println("");
+    System.out.println("");
+    System.out.println("Word 16");
+        for (int row = 0; row < 4; row++) {
+            // System.out.println("");
+            for (int column = 0; column < 4; column++) {
+                if ((column == 2 && row == 0) || (column == 2 && row == 1) || (column == 2 && row == 2)) {
+                    System.out.println(mdArray[row][column]);
+            }
+        }
+    }
+
+    // System.out.println("");
+    System.out.println("");
+    System.out.println("Word 17");
+        for (int row = 3; row >= 0; row--) {
+            // System.out.println("");
+            for (int column = 3; column >= 0; column--) {
+                if ((column == 1 && row == 0) || (column == 1 && row == 1) || (column == 1 && row == 2)) {
+                    System.out.println(mdArray[row][column]);
+            }
+        }
+    }
+
+    System.out.println("");
+    System.out.println("Word 18");
         for (int row = 0; row < 4; row++) {
             System.out.println("");
-            for (int column = 0; column < 4; column++) {
-                if ((column == 0 && row == 3 || column == 1 && row == 2 || column == 2 && row == 1 || column == 3 && row == 0)) {
+            for (int column = 3; column >= 0; column--) {
+                if ((column == 0 && row == 3 || column == 1 && row == 2 || column == 2 && row == 1)) {
                     System.out.print(mdArray[row][column]);
             }
             else
+            {
+                System.out.print(" ");
+            }
+        }
+    }
+
+    System.out.println("");
+    System.out.println("");
+    System.out.println("Word 19");
+        for (int row = 0; row < 4; row++) {
+            System.out.println("");
+            for (int column = 0; column < 4; column++) {
+                if ((column == 0 && row == 0 ) || (column == 1 && row == 1 ) || (column == 2 && row == 2 )){
+                    System.out.print(mdArray[row][column]);
+            }
+            else 
+            {
+                System.out.print(" ");
+            }
+        }
+    }
+
+    // System.out.println("");
+    System.out.println("");
+    System.out.println("Word 20");
+        for (int row = 0; row < 4; row++) {
+            System.out.println("");
+            for (int column = 0; column < 4; column++) {
+                if ((column == 0 && row == 1 ) || (column == 0 && row == 2 ) || (column == 1 && row == 2 )){
+                    System.out.print(mdArray[row][column]);
+            }
+            else 
+            {
+                System.out.print(" ");
+            }
+        }
+    }
+
+    System.out.println("");
+    System.out.println("Word 21");
+        for (int row = 0; row < 4; row++) {
+            System.out.println("");
+            for (int column = 0; column < 4; column++) {
+                if ((column == 0 && row == 0 ) || (column == 0 && row == 1 ) || (column == 1 && row == 2 ) || (column == 1 && row == 3 )){
+                    System.out.print(mdArray[row][column]);
+            }
+            else 
             {
                 System.out.print(" ");
             }
